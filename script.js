754ffe97f34c2999d92d44308f56df9c98539c68
Waterlilyrr <noemict94@hotmail.it>
2022-06-28 19:19:35 +0200

    chore: styling

--- script.js
+++ script.js
@@ -16,6 +16,8 @@ const carrello = newEl("p");
 
 titleEl.innerText = "Nome utente: ";
 titleEl2.innerText = "Nome utente: ";
+contentEl.innerText = nomeUtente + ";";
+contentEl2.innerText = nomeUtente;
 
 pezziInEccesso.innerText = "Prodotti in eccesso(>200 pezzi):";
 document.body.append(navbarEl, pezziInEccesso, productList, divfooter);
@@ -24,8 +26,6 @@ divfooter.append(footerEl);
 footerEl.append(titleEl2, contentEl2);
 
 localStorage.setItem("username", nomeUtente);
-contentEl.innerText = nomeUtente + ";";
-contentEl2.innerText = nomeUtente;
 
 navbarEl.className = "navbar_El";
 divfooter.className = "divfooter";
@@ -33,11 +33,10 @@ footerEl.className = "footer_El";
 productList.className = "product_List";
 pezziInEccesso.className = "pezziInEccesso";
 carrello.className = "carrello";
-
-titleEl.style = `margin-top: 0px; padding-top: 5px`;
-contentEl.style = `margin-top: 0px; padding: 5px 0 0 10px;`;
-titleEl2.style = `margin-top: 0px; padding-top: 5px`;
-contentEl2.style = `margin-top: 0px; padding: 5px 0 0 10px;`;
+titleEl.className = "title_El";
+titleEl2.className = "title_El2";
+contentEl.className = "content_El";
+contentEl2.className = "content_El2";
 
 fetch(BASE_URL)
   .then((res) => res.json())
